bf004979fa78738762f2d7bfba1030ccfd28beaa
Jordan McCrank <81048903+InsertFunny@users.noreply.github.com>
2022-05-20 20:10:09 +0000

    Finished

--- main.py
+++ main.py
@@ -5,60 +5,65 @@ movies = {"Title":"Back to the Future","Year":"1985","Rated":"PG","Released":"03
 #movie_title = 'Back To The Future'
 #data = f'httpd://www.ombapi.com/?apikey={API_KEY}&t={movie_title}'
 #response = requests.get(data).json()
-#Could not make this work
+#Could not make this working. However if it were intergrated into the code I would be able to make a 'blog'for any movie Even if it is missing something.
+
 
 def whatIs(m, t):
   try:
-  
     if t == 'Title':
       autoSentence(m, t, f"{m[t]}")
-    if t == 'Released':
+    elif t == 'Released':
       temp = movies['Released']
       temp = temp[7:11]
       if(temp == "2022"):
-        autoSentence(m,t,f"This movie was released was released {m[t]} which was released this year.")
+        autoSentence(t,f"{m['Title']} was released was released {m[t]} which was released this year.")
       else:
-        autoSentence(m,t,f"This movie was released was released {m[t]}.")
-    if t == 'Actors':
+        autoSentence(t,f"{m['Title']} was released was released {m[t]}.")
+    elif t == 'Actors':
       temp = getRandom("acclaiemed", "popular", " ")
-      autoSentence(m, t, f"This movie stars {temp} actors: {m[t]}.")
-    if t == 'Plot':
-      autoSentence(m, t, f"The {t} of the movie is: {m[t]}")
-    if t == 'Director':
+      autoSentence(t, f"{m['Title']} stars {temp} actors: {m[t]}.")
+    elif t == 'Plot':
+      autoSentence(t, f"The {t} of the {m['Title']} is: {m[t]}")
+    elif t == 'Director':
       temp = getRandom("brilliant", "dashing", "marvelous")
-      autoSentence(m, t, f"This {temp} movie's {t} is {m[t]}.")
-    if t == 'Metascore':
+      autoSentence(t, f"The {temp} {t} {m[t]} directed {m['Title']}.")
+    elif t == 'Metascore':
       temp = m[t]
       if int(temp) == 100:
-        autoSentence(m, t, f"This movies has a perfect {t} score of {temp}%!")
-      elif int(temp[0]) < 3:
-        autoSentence(m, t, f"This movie has a terrible {t} level is {temp}%. Spend your time on another film.")
-      elif int(temp[0]) > 6:
-        autoSentence(m, t, f"The {t} of this film is {temp}. You should probably watch this.")
-    if t == 'Awards':
-      autoSentence(m, t, f"The movies has recived {m[t]} for {t}.")
-    if t =='Writer':
+        autoSentence(t, f"{m['Title']} has a perfect {t} score of {temp}%!")
+      elif int(temp[0]) <= 6:
+        autoSentence(t, f"{m['Title']} has a terrible {t} level is {temp}%. Spend your time on another film.")
+      elif int(temp[0]) >= 7:
+        autoSentence(t, f"The {t} of {m['Title']} is {temp}. I siggest that you watch this.")
+    elif t == 'Awards':
+      autoSentence(t, f"{m['Title']} has recived {m[t]} for {t}.")
+    elif t =='Writer':
       temp = getRandom("talented", "fabulous", " ")
-      autoSentence(m, t, f"Thr {t} for this movie is the {temp} {m[t]}.")
+      autoSentence(t, f"The {t} of {m['Title']} is the {temp} {m[t]}.")
   except:
-    # This code is useless
+    # This code is useless. Just so that in case soemthing fails, which it souldn't anyways.
     b = 1
-  
+     
     
-  
-    
-    
-def autoSentence(m, t, sentence):
+def autoSentence(t, sentence):
   print(t + ": " + sentence)
 
-def blog(movies):
-  headLine(movies)
-  for thing in movies:
-    whatIs(movies, thing)
 
+def blog(m):
+  headLine(m)
+  for thing in m:
+    temp = whatIs(m, thing)
+  print()
+  temp = m['Metascore']
+  if (temp) == 100:
+    autoSentence("My review", f"{m['Title']} was a amazing movie. I would definetly recommend it. The amazing acting from {m} is amazing. The plot written by {m['Writer']} was greate and cinematography by {m['Director']} was superb.")
+  elif int(temp[0]) <= 6:
+    autoSentence("My review", f"{m['Title']} was a terrible movie. All personel that worked on this movie should be ashamed. The disgusting plot written by {m['Writer']} and directed by vile {m['Director']}. The actors performance was terrible, {m['Actors']}. Don't think about watching this movie unless you love wathcing dumpster fires.")
+  elif int(temp[0]) >= 7:
+    autoSentence("My review", f"{m['Title']} this movie is pretty acclaimed. The director {m['Director']} should be very happy with this movie. {m['Actors']} did a great job in this film and really support the writing of {m['Writer']}. If this movie sounds intresting I would highly suggest watching it.")
 
 def headLine(m):
-  temp = getRandom(f"Here are some fact about {m['Title']}!", f"Let's look back on {m['Title']}!", f"{m['Title']} is a classic!")
+  temp = getRandom(f"HERE ARE SOEM FACTS ABOUT {m['Title']}!", f"LET'S LOOK BACK ON {m['Title']}!", f"{m['Title']} IS A CLASSIC!")
   print(temp)
   print()
 
@@ -79,5 +84,6 @@ def getRandom(c1, c2, c3):
     elif ran == 1:
       return c2
 
-      
+
+#This was very difficult. I wanted to make sentences that go automatically as long as the movie has a certain peramiter. I'm not sure if this is what you wanted. 
 blog(movies)
